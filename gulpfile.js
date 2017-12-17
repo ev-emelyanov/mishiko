9405d1db9efa2b41544f86fb3473a3a21b7ad541
Eugen Emelyanov <emel.yanov@mail.ru>
2017-12-18 00:50:52 +0700

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csslint = require('gulp-csslint'); // https://www.npmjs.com/package/gulp-csslint
 
 gulp.task("style", function() {
     gulp.src("less/style.less")
@@ -29,4 +30,10 @@ gulp.task("serve", ["style"], function() {
 
     gulp.watch("less/**/*.less", ["style"]);
     gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task('css', function() {
+    gulp.src('client/css/*.css')
+        .pipe(csslint())
+        .pipe(csslint.formatter());
 });
