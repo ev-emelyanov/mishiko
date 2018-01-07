930a4221ff779c82bb04bc81100e25eccb9434e1
Eugen Emelyanov <emel.yanov@mail.ru>
2018-01-08 00:23:04 +0700

    mobile menu without js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 var topMenu = document.querySelector('.main-nav__wrapper');
+var mainNav = document.querySelector('.main-nav');
+mainNav.classList.remove('main-nav--nojs');
+topMenu.classList.remove('main-nav__wrapper--closed');
+topMenu.classList.toggle('hidden');
 
 jsTopBtn.addEventListener('click', function (event) {
     this.classList.toggle('main-nav__toggler--close');
