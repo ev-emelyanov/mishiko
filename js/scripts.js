b58908803f5f11b40bbe5e41ae08434cdd005181
Eugen Emelyanov <emel.yanov@mail.ru>
2018-01-09 01:51:46 +0700

    top menu tablet

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 var topMenu = document.querySelector('.main-nav__wrapper');
 var mainNav = document.querySelector('.main-nav');
 mainNav.classList.remove('main-nav--nojs');
-topMenu.classList.remove('main-nav__wrapper--closed');
-topMenu.classList.toggle('hidden');
+topMenu.classList.remove('main-nav__wrapper--opened');
+topMenu.classList.add('main-nav__wrapper--closed');
 
 jsTopBtn.addEventListener('click', function (event) {
     this.classList.toggle('main-nav__toggler--close');
     this.classList.toggle('main-nav__toggler--open');
-    topMenu.classList.toggle('hidden');
+    if (topMenu.classList.contains('main-nav__wrapper--opened')) {
+        topMenu.classList.remove('main-nav__wrapper--opened');
+        topMenu.classList.add('main-nav__wrapper--closed');
+    } else {
+        topMenu.classList.add('main-nav__wrapper--opened');
+        topMenu.classList.remove('main-nav__wrapper--closed');
+    }
     // console.log('btn: ', this);
 });
